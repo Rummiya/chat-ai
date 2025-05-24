7a6f9064fcfb859b6298461b38054003be61e576
Rummiya <rumibashirova@gmail.com>
2025-05-24 13:37:44 +0600

    refactor(spinner): add className to props

--- src/components/shared/spinner.tsx
+++ src/components/shared/spinner.tsx
@@ -1,5 +1,12 @@
-export const Spinner = () => {
+import { cn } from '@/lib/utils/cn';
+
+export const Spinner = ({ className }: { className?: string }) => {
 	return (
-		<div className='max-w-10 max-h-10 size-full border-4 border-sky-700 border-t-transparent rounded-full animate-spin'></div>
+		<div
+			className={cn(
+				'max-w-10 max-h-10 size-full border-4 border-sky-700 border-t-transparent rounded-full animate-spin',
+				className
+			)}
+		></div>
 	);
 };
